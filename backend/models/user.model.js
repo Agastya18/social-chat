0f06438e7a983dab6dd3791d4b668693c8416f03
agastya gaur <agastyagaur17@gmail.com>
2024-03-08 23:34:36 +0530

    Add connectDB and authMiddleware files

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -31,6 +31,31 @@ const userSchema = new mongoose.Schema({
     timestamps: true
 });
 
+
+// password hashing before saving
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")){
+        next()
+    }
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+// matching password function
+userSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+//genrating access token for user
+userSchema.methods.getAccessToken = function(){
+    return jwt.sign({id: this._id}, process.env.TOKEN_SECRET, {expiresIn: "15d",
+   
+   
+    },
+    
+    )
+}
+
+
  const User= mongoose.model('User', userSchema);
 
 export default User;
